bce0e442ee71a4cf8e5954c27b44bc88ff0a8eeb
Franz <gualambo@gmail.com>
2020-04-05 21:43:29 -0500

    Actualizando para nuevas versiones de Jquery

--- validCampoFranz.js
+++ validCampoFranz.js
@@ -1,3 +1,11 @@
 /****** Creado por Franz Gualambo ***************/
 /***** Email : gualambo@gmail.com  *****************/
-(function(a){a.fn.validCampoFranz=function(b){a(this).on({keypress:function(a){var c=a.which,d=a.keyCode,e=String.fromCharCode(c).toLowerCase(),f=b;(-1!=f.indexOf(e)||9==d||37!=c&&37==d||39==d&&39!=c||8==d||46==d&&46!=c)&&161!=c||a.preventDefault()}})}})(jQuery);
+ (function ( a ) {
+            a.fn.validCampoFranz=function(b){
+            a(this).on({keypress:function(a){
+            var c=a.which,
+            d=a.keyCode,
+            e=String.fromCharCode(c).toLowerCase(),
+            f=b;
+        (-1!=f.indexOf(e)||9==d||37!=c&&37==d||39==d&&39!=c||8==d||46==d&&46!=c)&&161!=c||a.preventDefault()
+        }})}}( jQuery ));
